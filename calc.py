4fe94237ad6e737f8cdf6a2e999594df72101c38
mmariano <marcusney@gmail.com>
2019-02-12 10:39:31 -0300

    Divide Function

--- calc.py
+++ calc.py
@@ -1,3 +1,15 @@
+"""
+Git and GitHub
+
+Git Tutorial for Beginners_ Command-Line Fundamentals
+
+Homepage and documentation:
+https://github.com/python-marcus-mariano/git-fundamental
+
+Copyright (c) 2018, Marcus Mariano.
+License: MIT (see LICENSE for details)
+"""
+
 def add(x, y):
     pass
 
@@ -8,7 +20,7 @@ def multiply(x, y):
     pass
 
 def divide(x, y):
-    pass
+    return x/y
 
 def square(x, y):
     pass
